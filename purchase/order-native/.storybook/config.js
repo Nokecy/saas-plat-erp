4b4d2954830452833a78be04dc4d0c2c28183b1e
caizhenxing <caizhenxing@gmail.com>
2017-07-31 21:51:58 +0800

    支持storybook

--- purchase/order-native/.storybook/config.js
+++ purchase/order-native/.storybook/config.js
@@ -1,34 +1,9 @@
-import {configure} from '@storybook/react';
-import winflex from './flex';
+import { configure } from '@storybook/react';
 
-function flex() {
-  winflex(100, 1);
-}
-
-function theme() {
-  let antdRequiredStyles = 'html,body,#root,div[data-reactroot]{width:100%;height:100%;}';
-  // if (Screen.width > Screen.sm) { antdRequiredStyles += 'html{font-size:
-  // 100PX;}'; } create stylesheet
-  const style = document.createElement('style');
-  style.type = 'text/css';
-  if (style.styleSheet) {
-    style.styleSheet.cssText = antdRequiredStyles;
-  } else {
-    style.appendChild(document.createTextNode(antdRequiredStyles));
-  }
-
-  // inject stylesheet
-  document.head.appendChild(style);
-}
+const req = require.context('../stories', true);
 
 function loadStories() {
-
-  flex();
-  theme();
-
-  require('../stories/App.js');
-
-  // You can require as many stories as you need.
+  req.keys().forEach(req);
 }
 
 configure(loadStories, module);
--- purchase/order-native/stories/App.js
+++ purchase/order-native/stories/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { App } from 'saas-plat-native-core';
-import '../src';
+//import '../src';
 
-storiesOf('XS').add('Create', () => (
-  <App path='/erp/purchase/order/create'/>
-)).add('Detail', () => (
-  <App path='/erp/purchase/order/detail/1'/>
-)).add('Edit', () => (
-  <App path='/erp/purchase/order/edit/2'/>
-)).add('List', () => (
-  <App path='/erp/purchase/order/list'/>
-));
+console.log(App)
+
+// storiesOf('XS').add('Create', () => (
+//   <App path='/erp/purchase/order/create'/>
+// )).add('Detail', () => (
+//   <App path='/erp/purchase/order/detail/1'/>
+// )).add('Edit', () => (
+//   <App path='/erp/purchase/order/edit/2'/>
+// )).add('List', () => (
+//   <App path='/erp/purchase/order/list'/>
+// ));
