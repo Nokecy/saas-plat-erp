3b512e2dec7540de31bd8f66ac52e1644cc9d9fb
caizx <caizx@chanjet.com>
2017-08-18 16:16:47 +0800

    微调

--- purchase/purchase/test/config/index.js
+++ purchase/purchase/test/config/index.js
@@ -1,4 +1,13 @@
 import testing from 'saas-plat-server/config/testing';
+import { name } from '../../package.json';
+
+global.publish = async(command, data) => {
+  await saasplat.publish(name, command, data);
+}
+
+global.model = (name) => {
+  return saasplat.model(name);
+}
 
 export default function() {
   before(() => testing({ password: '123456' }));
--- purchase/purchase/test/purchasing_business_test.js
+++ purchase/purchase/test/purchasing_business_test.js
@@ -1,13 +1,12 @@
 // 进货业务
 import { expect } from 'chai';
 import init from './config';
-import { name } from '../package.json';
 
 describe('进货业务', function() {
   init();
 
   it('普通进货', async function() {
-    await saasplat.publish(name, 'purchase', {});
+    await publish('purchase', {});
   });
 
   it('普通退货', function() {
